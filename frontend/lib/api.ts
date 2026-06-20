49dc3f5cc03c7e1386494d75bc68ac58739db1d3
Martín Ponce <martinp88@gmail.com>
2026-06-20 01:53:28 -0300

    add api interceptor

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -6,3 +6,12 @@ export const api = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error('API Error', error);
+
+    return Promise.reject(error);
+  },
+);
